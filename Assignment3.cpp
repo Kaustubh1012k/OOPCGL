63edb602edf23cf1fbe7e574ccb52af6f06ae54c
Kaustubh Korpe <114090049+KaustubhK0@users.noreply.github.com>
2022-12-23 12:01:22 +0530

    Add files via upload

---
+++ Assignment3.cpp
@@ -0,0 +1,172 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+class Publication
+{
+    string title;
+    float price;
+
+public:
+
+Publication(){
+    title=" ";
+    price=0;
+}
+    void setData()
+    {
+        cout << "Enter the Title  " << endl;
+        cin >> title;
+        getline(cin, title);
+        cout << "Enter the Price " << endl;
+        cin >> price;
+    }
+    void displayData()
+    {
+        cout<<"\nInformation: ";
+        cout << "Title of Publication: " << title << endl;
+        cout << "Price: " <<price<< endl;
+    }
+};
+
+class Book : public Publication
+{
+    int page_count;
+
+public:
+    Book()
+    {
+        page_count = 0;
+    }
+
+    void setBook()
+    {
+        Publication::setData();
+        cout<<endl;
+        cout << "Enter Page Count: " << endl;
+        cin >> page_count;
+    }
+
+    void displayBook()
+    {
+        Publication::displayData();
+        cout<<endl;
+        try{
+            
+            
+            if (page_count < 0)
+            {
+                throw page_count;
+            }
+            else
+            {
+                cout << "Page Count is " << page_count << endl;
+            }
+        }
+        catch (int x)
+        {
+            cout << "Invalid Pages" <<x<< endl;
+            page_count = 0;
+        }
+    }
+};
+
+class tape : public Publication
+{
+    float time;
+
+public:
+    tape()
+    {
+        time = 0;
+    }
+    void setTape()
+    {
+        Publication::setData();
+        cout<<endl;
+        cout << "Enter Time in minutes: " << endl;
+        cin >> time;
+        
+    }
+
+    void displayTape()
+    {
+        Publication::displayData();
+        cout<<endl;
+        try
+        {
+            if (time < 0)
+            {
+                throw time;
+            }
+        }
+        catch (float p)
+        {
+            cout << "Invalid Time" <<p<< endl;
+            time = 0;
+        }
+        cout << "Time is: " << time << endl;
+    }
+};
+
+int main()
+{
+
+    Book b[10];
+    tape t[10];
+
+    int choice=0, bcount = 0, tcount = 0;
+
+    do
+    {
+        cout << "\n1.Add Book " << endl;
+        cout << "2.Add Tape " << endl;
+        cout << "3.Display Book " << endl;
+        cout << "4.Display Tape " << endl;
+        cout << "5.Exit " << endl;
+        cout << "\nEnter The choice " << endl;
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            cout << "Add Book " << endl;
+            b[bcount].setBook();
+            bcount++;
+            break;
+
+        case 2:
+            cout << "Add Tape" << endl;
+            t[tcount].setTape();
+            tcount++;
+            break;
+
+        case 3:
+            cout << "Display Book " << endl;
+            for (int i=0; i < bcount; i++)
+            {
+                b[i].displayBook();
+            }
+            break;
+
+        case 4:
+            cout << "Display Tape" << endl;
+            for (int j=0; j <tcount; j++)
+            {
+                t[j].displayTape();
+            }
+            break;
+
+        case 5:
+            cout << "Program Exited " << endl;
+            exit(0);
+            break;
+
+        default:
+            cout << "Invalid" << endl;
+            break;
+        }
+    } while (choice != 5);
+
+    return 0;
+}
