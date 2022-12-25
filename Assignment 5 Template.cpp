0c771cd5ac464646e4a789a372585cb489124e6d
Kaustubh Korpe <114090049+KaustubhK0@users.noreply.github.com>
2022-12-25 21:44:04 +0530

    Update Assignment 5 Template.cpp

--- Assignment 5 Template.cpp
+++ Assignment 5 Template.cpp
@@ -21,7 +21,7 @@ T selectionsort(){
             if(array[i]>array[j]){
                 temp=array[i];
                 array[i]=array[j];
-                array[i]=temp;
+                array[j]=temp;
             }
         }
     }
